cda5931a74772165471b5700fa5000c4105a5af3
ENZO KAWAKAMI MORETTI <enzo.moretti3@senaisp.edu.br>
2026-04-29 16:34:34 -0300

    Exercicio2(For)

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Ex1 {
+    void main (){
+        Scanner sc = new Scanner(System.in);
+        IO.println("Digite um numero e mostrarei seu fatorial: ");
+        int num2 = sc.nextInt();
+        int valor = 1;
+        for(int num1 = 1;num1 <= num2;num1++){
+            IO.println(num1);
+            valor = valor*num1;
+        }
+        IO.println(valor);
+    }
 }
